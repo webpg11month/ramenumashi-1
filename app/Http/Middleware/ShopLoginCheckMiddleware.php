51a5341efac11a2d568abbf706ccaeef81539835
msh-pg <https://V4U8mvDLyjBMtzRwd9Xs:msh-pg@github.com/mash-pg/RegisterSalon.git>
2021-01-03 21:04:39 +0900

    お店のミドルウェア処理追加
    add_mash

---
+++ app/Http/Middleware/ShopLoginCheckMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Closure;
+
+class ShopLoginCheckMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = 'shop')
+    {
+        
+        if (!Auth::guard($guard)->check()) {
+            Log::info('test2');
+               return redirect('/message/resultshoplogin');
+        }
+        Log::info('test1');
+        return $next($request);
+    }
+}
