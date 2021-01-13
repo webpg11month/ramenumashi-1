ffc26e386e42b73421a977fbbb28c6c1613a6c89
mhs-pg <ymanshou@gmail.com>
2021-01-13 18:51:22 +0900

    idカラム削除 reserve_idとuser_idで複合キー設定済 edit_mash

--- database/migrations/2021_01_13_093338_create_reserve_tb_table.php
+++ database/migrations/2021_01_13_093338_create_reserve_tb_table.php
@@ -14,8 +14,14 @@ class CreateReserveTbTable extends Migration
     public function up()
     {
         Schema::create('reserve_tb', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->string('reserve_id',16);               
+            $table->string('user_id',255);
+            $table->integer('number')->unique();    
+            $table->string('reserve_time',255);          
+            $table->enum('dlflag', ['1','2','3']);
             $table->timestamps();
+
+            $table->primary('reserve_id','user_id');
         });
     }
 
