8498a16bb9852199a230b40baf420e27ac1991b1
mhs-pg <ymanshou@gmail.com>
2021-01-14 20:53:25 +0900

    テーブル追加 edit_mash

---
+++ app/Table/Reserve.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Table;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Reserve extends Authenticatable
+{
+    protected $table = 'reserve_tb';
+
+    protected $fillable = [
+        'reserve_id',
+        'user_id',
+        'number',
+        'reserve_time',
+        'dlflag'
+    ];
+}
