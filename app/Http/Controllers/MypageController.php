41f9cf1a0d04736566b63bd65d9745694e49a2b0
mhs-pg <ymanshou@gmail.com>
2021-01-19 05:09:39 +0900

    お気に入り処理追加 お気に入りテーブル作成 お気に入り登録 お気に入り削除 お気に入り画面 お気に入り解除画面 edit_add_mash

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Table\User;
+use App\Table\Okini;
 use App\Table\Reserve;
 use Illuminate\Support\Facades\Log;
 use Auth;
@@ -25,15 +26,27 @@ class MypageController extends Controller
                 ->join('shop_tb','reserve_tb.shop_id','=','shop_tb.shop_id')
                 ->join('user_tb','reserve_tb.user_id','=','user_tb.user_id')
                 ->orderBy('reserve_tb.created_at', 'desc')
-                ->paginate(15);
+                ->paginate(5);
         
-        return view('/mypage',compact('reserves','user'));
+        //お気に入り一覧
+        $okinis = DB::table('okini_tb')
+                //カラムデータ処理
+                // ->select('reserve_tb.reserve_id','shop_tb.img','shop_tb.shop_name','shop_tb.shop_id','reserve_tb.dlflag')
+                ->where('okini_tb.dlflag',1)
+                ->where('okini_tb.user_id',$user_id)
+                ->join('shop_tb','okini_tb.shop_id','=','shop_tb.shop_id')
+                ->join('user_tb','okini_tb.user_id','=','user_tb.user_id')
+                ->orderBy('okini_tb.created_at', 'desc')
+                ->paginate(5);
+
+        return view('/mypage',compact('reserves','okinis','user'));
     }
 
-    public function edit(Request $req){
+    //予約詳細一覧
+    public function detail(Request $req){
         $shopinfo = $req->all();
         //ユーザーID $req['user_id']でも取得可能だが、ログイン認証の方がよい
-        $user = Auth::user(); 
+        $user = Auth::user();
         $user_id = $user->user_id;
         //予約ID
         $reserve_id = $req['reserve_id'];
@@ -46,5 +59,59 @@ class MypageController extends Controller
         ->join('user_tb','reserve_tb.user_id','=','user_tb.user_id')->first();
         
         return view('/mypage/detail',compact('reserves'));
+    }
+
+    //キャンセル処理
+    public function delete(Request $req){
+        $reserve_id = $req['reserve_id'];
+        Reserve::where('reserve_id',$reserve_id)->update(['dlflag'=>3]);
+        return view('/message/resultcancel');
+    }
+
+    //お気に入り登録
+    public function okini(Request $req){
+        $user = Auth::user();
+        $user_id = $user->user_id;
+        $shop_id = $req['shop_id'];
+
+        $okiniinfos = Okini::where('shop_id', $req['shop_id'])->exists();
+        if(!$okiniinfos){
+            $user_infos = Okini::create([
+                'shop_id' => $req['shop_id'],
+                'user_id' => $user_id,
+                'dlflag'=> 1
+            ]);
+        }else {
+            Log::info('すでにお気に入り登録されています');
+            return redirect()->back();
+        }
+
+        return view('/message/resultokini');
+    }
+
+    //お気に入りお店詳細一覧
+    public function okiniDetail(Request $req){
+        $shopinfo = $req->all();
+        //ユーザーID $req['user_id']でも取得可能だが、ログイン認証の方がよい
+        $user = Auth::user(); 
+        $user_id = $user->user_id;
+        //お気に入りID
+        $okini_id = $req['okini_id'];
+
+        $okinis = DB::table('okini_tb')
+        ->where('okini_tb.dlflag',1)
+        ->where('okini_tb.user_id',$user_id)
+        ->where('okini_tb.okini_id',$okini_id)
+        ->join('shop_tb','okini_tb.shop_id','=','shop_tb.shop_id')
+        ->join('user_tb','okini_tb.user_id','=','user_tb.user_id')->first();
+        
+        return view('/mypage/okinidetail',compact('okinis'));
+    }
+
+    //お気に入り解除処理
+    public function okiDelete(Request $req){
+        $okini_id = $req['okini_id'];
+        Okini::where('okini_id',$okini_id)->update(['dlflag'=>3]);
+        return view('/message/resultokinicancel');
     }
 }
--- resources/views/mypage/okinidetail.blade.php
+++ resources/views/mypage/okinidetail.blade.php
@@ -7,18 +7,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="ラーメン検索とラーメン空き情報を確認できる">
   <!-- CSS読込み -->
-  <link href="css/header.css" rel="stylesheet">
-  <link href="css/main.css" rel="stylesheet">
-  <link href="css/footer.css" rel="stylesheet">
+  <link href="../css/header.css" rel="stylesheet">
+  <link href="../css/main.css" rel="stylesheet">
+  <link href="../css/footer.css" rel="stylesheet">
   <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-  <link href="css/ress.css" rel="stylesheet">
+  <link href="../css/ress.css" rel="stylesheet">
   <!-- ファビコン -->
   <link rel="icon" type="img/png" href="img/favicon/favicon.png">
   <!-- タイトル -->
-  <title>RamenUmashi</title>
+  <title>RamenUmashi | okinidetail</title>
 </head>
-
-
 <body>
   <div class="wrapper-header">
     <header>
@@ -27,7 +25,7 @@
       <div id="ramen-home" class="big-bg">
         <div class="page-header wrapper">
           <a class="logo-flex" href="{{ url('/') }}">
-            <img class="fade-main" id="ramen-logo" src="img/logo/ramen-log.png" alt="">
+            <img class="fade-main" id="ramen-logo" src="../img/logo/ramen-log.png" alt="">
           </a>
           <a class="logo-flex" href="{{ url('/') }}">
             <h1  id="logo-font">
@@ -98,31 +96,46 @@
         </div>
       </div>
   </header>
-  <main class="wrapper-mypage">
-    <h1 class="mypage-head">予約一覧</h1>
-    <p class="mypage-login">{{$user['user_name']}}様ログイン中</p>
-    <div class="wrapper grid">
-      @foreach ($reserves as $reserve)
-      <div class="item">
-        <p>{{ $reserve->shop_name}}</p>
-          <a href="{{action('MypageController@detail')}}?reserve_id={{ $reserve->reserve_id}}">
-            <img class="hover-index" src="{{asset('storage/image/'.$reserve->img)}}" alt="menu">
-          </a>
-      </div>
-      @endforeach
-    </div><!-- /.grid -->
-    {{$reserves->appends(request()->input())->render()}}
-    <h1 class="mypage-head">お気に入り一覧</h1>
-      <h1 class="mypage-head">マイページ</h1>
-      <div class="wrapper grid">
-        @foreach ($okinis as $okini)
-        <div class="item">
-          <p>{{ $okini->shop_name}}</p>
-          <a class="hover-index" href="{{action('MypageController@okiniDetail')}}?okini_id={{ $okini->okini_id}}"><img src="{{asset('storage/image/'.$okini->img)}}" alt="menu"></a>
+    <main class="wrapper-mypage">
+      <h1 class="mypage-head">お店情報</h1>
+      <form class="reig-center" action="{{action('MypageController@okiDelete')}}" method="get">
+        @csrf
+        <input type="hidden" name="okini_id" value="{{$okinis->okini_id}}">
+        <div class="main-regi-grid">
+          <div class="main-regi-item">
+            <label>
+              店名：
+            </label>
+          </div>
+          <div class="main-regi-item">
+            <p>{{$okinis->shop_name}}</p>
+          </div>
         </div>
-        @endforeach
-      </div><!-- /.grid -->
-      {{$okinis->appends(request()->input())->render()}}
+        <div class="main-regi-grid">
+          <div class="main-regi-item">
+            <label>
+              電話番号：
+            </label>
+          </div>
+          <div class="main-regi-item">
+            <p>{{$okinis->shop_tel}}</p>
+          </div>
+        </div>
+        <div class="main-regi-grid">
+          <div class="main-regi-item">
+           <label>
+              アクセス：
+            </label>
+          </div>
+          <div class="main-regi-item">
+            <p>{{$okinis->shop_address}}</p>
+          </div>
+        </div>
+        <div class="regi-submit">
+          <input type="submit" name="submit" value="お気に入り解除" class="button">
+        </div>
+      </form>
+    </div>
     </main>
     <footer>
       <nav class="footer-nav-list">
