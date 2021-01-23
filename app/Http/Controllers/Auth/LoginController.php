f0e79d26ccac000d510e24a6faaced904bb8bbc1
mhs-pg <ymanshou@gmail.com>
2021-01-24 05:49:21 +0900

    ログイン処理　パスワードとユーザーID両方可能 edit_mash

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,14 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Table\User;
+use Illuminate\Support\Facades\Log;
+use App\Http\Requests\UserLoginRequest;
+use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 class LoginController extends Controller
 {
     /*
@@ -33,25 +40,40 @@ class LoginController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
+    // public function __construct()
+    // {
+    //     //shop mash
+    //     $this->middleware('guest')->except('logout');
+    // }
     
     public function userLogin(UserLoginRequest $req){
         $data = $req->all();
-        $email =User::where('email',$req->email)->value('email');
-        $password1 = User::where('email',$req->email)->value('password');
+        Log::info($req->login);
+        $user_status = User::where('email',$req->login)->value('dlflag');
+        if(preg_match('|^[0-9a-z_./?-]+@([0-9a-z-]+\.)+[0-9a-z-]+$|', $req->login)){
+            $email =User::where('email',$req->login)->value('email');
+            $password1 = User::where('email',$req->login)->value('password');
+            Log::info($email);
+            $credentials = [
+                'email' => $email,
+                'password' => $req->password,
+            ];
+        }else {      
+            $user_id = User::where('user_id',$req->login)->value('user_id');
+            $password1 = User::where('user_id',$req->login)->value('password');
+            $credentials = [
+                'user_id' => $user_id,
+                'password' => $req->password,
+            ];   
+            Log::info($user_id);
+        }
 
-        $user_status = User::where('email',$req->email)->value('dlflag');
         if($user_status === '3'){
             Log::info('退会者');
             return redirect()->back();
         }
 
         if(Hash::check($req->password, $password1)){
-            $credentials = $req->only('email', 'password');
-
             Log::info($credentials);
             if(Auth::attempt($credentials)){
                 Log::info('ログイン成功');
